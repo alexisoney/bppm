00e1ef5f9bef38ed6067b0617e4b9a3d8ecce3ce
Alexis <alexis.oney@gmail.com>
2019-08-13 07:37:08 +0200

    Fix: navigation opening

--- src/components/navigation/navigation-view.js
+++ src/components/navigation/navigation-view.js
@@ -39,7 +39,7 @@ export default props => {
   }, []);
 
   useEffect(() => {
-    if (wrapper.current) {
+    if (wrapper.current && appeared) {
       wrapper.current.classList.add('navigation--appeared');
     }
   }, [appeared]);
