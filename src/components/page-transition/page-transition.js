1278e54a772277265392b816bd066f9f3589366e
Alexis <alexis.oney@gmail.com>
2019-08-22 22:49:07 +0200

    [FEATURE] Implement Story component

--- src/components/page-transition/page-transition.js
+++ src/components/page-transition/page-transition.js
@@ -1,15 +1,15 @@
-import React, {createRef, useState, useEffect} from 'react';
-import {navigate} from 'gatsby';
-import {TweenLite, TimelineLite, Power2} from 'gsap';
+import React, { createRef, useState, useEffect } from 'react';
+import { navigate } from 'gatsby';
+import { TweenLite, TimelineLite, Power2 } from 'gsap';
 import Lottie from 'lottie-react-web';
-import {TransitionGroup, Transition} from 'react-transition-group';
+import { TransitionGroup, Transition } from 'react-transition-group';
 
 import animation from '../../assets/arrows_loader.json';
 import animationLogo from '../../assets/logo_intro.json';
 
 export const PageTransitionContext = React.createContext();
 
-export default function PageTransition({children, path}) {
+export default function PageTransition({ children, path }) {
   const [appeared, setAppeared] = useState(false);
   const [canScroll, setCanScroll] = useState(false);
   const [loaded, setLoaded] = useState(false);
@@ -29,20 +29,6 @@ export default function PageTransition({children, path}) {
   };
   const ease = Power2.easeInOut;
 
-  // useEffect(() => {
-  //   if (window !== undefined && document !== undefined) {
-  //     document.body.style.overflow = 'hidden';
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   if (window !== undefined && document !== undefined) {
-  //     if (canScroll) {
-  //       document.body.style.overflow = '';
-  //     }
-  //   }
-  // }, [canScroll]);
-
   return (
     <TransitionGroup component={null}>
       <Transition
@@ -61,7 +47,7 @@ export default function PageTransition({children, path}) {
               <div className='page-transition__intro-lottie'>
                 <Lottie
                   isPaused={!toggleIntro}
-                  options={{animationData: animationLogo, autoplay: false, loop: false}}
+                  options={{ animationData: animationLogo, autoplay: false, loop: false }}
                 />
               </div>
             </div>
@@ -70,11 +56,11 @@ export default function PageTransition({children, path}) {
             <Lottie
               width={200}
               isPaused={!toggleLoader}
-              options={{animationData: animation, autoplay: false, loop: false}}
+              options={{ animationData: animation, autoplay: false, loop: false }}
             />
           </div>
           <div className='page-transition__children' ref={wrapper}>
-            <PageTransitionContext.Provider value={{appeared: appeared, navigate: triggerExit, canScroll: canScroll}}>
+            <PageTransitionContext.Provider value={{ appeared: appeared, navigate: triggerExit, canScroll: canScroll }}>
               {children}
             </PageTransitionContext.Provider>
           </div>
@@ -83,25 +69,21 @@ export default function PageTransition({children, path}) {
     </TransitionGroup>
   );
 
-  // function blockScroll() {
-  //   window.scrollTo(0, 0);
-  // }
-
   function triggerExit(e) {
     if (wrapper && wrapper.current) {
       e.preventDefault();
       setCanScroll(false);
 
       const link = e.currentTarget.pathname;
-      const tl = new TimelineLite({onComplete: () => navigate(link)});
+      const tl = new TimelineLite({ onComplete: () => navigate(link) });
 
-      tl.to(wrapper.current, speed.exit, {opacity: 0, ease: ease});
+      tl.to(wrapper.current, speed.exit, { opacity: 0, ease: ease });
     }
   }
 
   // React Transition Group
   function onEnter() {
-    if (wrapper && wrapper.current) TweenLite.set(wrapper.current, {opacity: 0});
+    if (wrapper && wrapper.current) TweenLite.set(wrapper.current, { opacity: 0 });
   }
 
   function onEntering() {
@@ -113,10 +95,10 @@ export default function PageTransition({children, path}) {
         .delay(0.4)
         .call(setToggleIntro, [true])
         .add(displayPage())
-        .to(intro.current, 0.6, {height: '0vh', ease: Power2.easeInOut}, 4.2)
-        .call(setAppeared,[true],null,'-=0.2')
-        .call(setLoaded,[true])
-        .call(setCanScroll,[true]);
+        .to(intro.current, 0.6, { height: '0vh', ease: Power2.easeInOut }, 4.2)
+        .call(setAppeared, [true], null, '-=0.2')
+        .call(setLoaded, [true])
+        .call(setCanScroll, [true]);
     } else {
       tl.delay(speed.exit)
         .add(displayLoader(), `-=${speed.overlap}`)
@@ -133,9 +115,9 @@ export default function PageTransition({children, path}) {
 
     return tl
       .call(setToggleLoader, [true])
-      .to(el, speed.loader, {opacity: 1, ease: ease})
-      .to(el, speed.loader, {opacity: 0, ease: ease}, speed.loading - speed.loader)
-      .set(el, {display: 'none'})
+      .to(el, speed.loader, { opacity: 1, ease: ease })
+      .to(el, speed.loader, { opacity: 0, ease: ease }, speed.loading - speed.loader)
+      .set(el, { display: 'none' })
       .call(setToggleLoader, [false]);
   }
 
@@ -145,7 +127,7 @@ export default function PageTransition({children, path}) {
 
     // prettier-ignore
     return tl
-      .set(el, {position: '', width: '', opacity: 0})
-      .to(el, speed.enter, {opacity: '1', ease: ease});
+      .set(el, { position: '', width: '', opacity: 0 })
+      .to(el, speed.enter, { opacity: '1', ease: ease });
   }
 }
--- src/components/story/story-view.js
+++ src/components/story/story-view.js
@@ -1,73 +1,151 @@
-import React, {useState, useEffect, useRef} from 'react';
-import Lottie from 'lottie-react-web';
+import React, { useState, useEffect, useRef } from 'react';
+import { Power2, TweenLite, TimelineLite } from 'gsap';
+import lottie from 'lottie-web';
 import ReactMarkdown from 'react-markdown';
 
 import arrows from '../../assets/arrows_red.svg';
-import illustration from '../../assets/gestion_projet.json';
+import illustrationJSON from '../../assets/gestion_projet.json';
+import { breakpoints } from '../../variables';
+
+const isBrowser = typeof window !== undefined;
 
 const Story = props => {
-  const {chapters} = props.blok;
+  const wrapper = useRef();
+  const scene = useRef();
+  const illustration = useRef();
 
-  const [playLottie, setPlayLottie] = useState(false);
+  let wrapperAbsoluteTop, wrapperAbsoluteBottom, chapterHeight;
+  let chapters;
+  let svgAnimation;
 
-  const element = useRef();
+  const svgAnimationSegments = [[0, 150], [150, 315], [315, 470], [470, 930]];
 
   useEffect(() => {
-    if (element.current) {
-      const el = element.current;
+    if (isBrowser) {
+      svgAnimation = lottie.loadAnimation({
+        container: illustration.current,
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        animationData: illustrationJSON,
+      })
 
-      const onIntersection = entries => {
-        for (const entry of entries) {
-          if (entry.isIntersecting) {
-            setPlayLottie(true);
-          }
-        }
-      };
-      const observer = new IntersectionObserver(onIntersection, {threshold: 0.1});
-      observer.observe(el);
+      initComponent();
+
+      window.addEventListener('scroll', handleScroll);
+      window.addEventListener('resize', initComponent);
+
     }
+
+    return () => {
+      if (isBrowser) {
+        window.removeEventListener('scroll', handleScroll);
+        window.removeEventListener('resize', initComponent);
+      }
+    };
   }, []);
 
-  if (!chapters && chapters.length < 1) {
+  if (!props.blok && props.blok.chapters.length < 1) {
     return null;
   }
+
   return (
-    <div ref={element} className='story'>
-      {chapters.map((chapter, index) => (
-        <div className='story__chapter' key={chapter._uid}>
-          <div className='story__illustration'>
-            <Lottie
-              isPaused={!playLottie}
-              // segments={[0, 150]}
-              // forceSegment
-              options={{
-                animationData: illustration,
-                autoplay: false,
-                loop: true,
-              }}
-            />
+    <div ref={wrapper} className='story'>
+      <div ref={scene} className='story__scene'>
+        <div ref={illustration} className='story__illustration' />
+        {props.blok.chapters.map(chapter => (
+          <div className='story__chapter' key={chapter._uid}>
+            <div className='story__content'>
+              <h3 className='story__title'>{chapter.title}</h3>
+              <ul className='story__text'>{markdownToList(chapter.content)}</ul>
+            </div>
           </div>
-          <div className='story__content'>
-            <h3 className='story__title'>{chapter.title}</h3>
-            <ul className='story__text'>{markdownToList(chapter.content)}</ul>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
+
+  function handleScroll() {
+    if (window.innerWidth >= breakpoints.medium) {
+      const scrollYTop = window.pageYOffset;
+      const scrollYBottom = window.pageYOffset + window.innerHeight;
+
+      if (scrollYBottom >= wrapperAbsoluteBottom) {
+        window.requestAnimationFrame(() => {
+          TweenLite.set(scene.current, { position: 'sticky' });
+        })
+      } else if (scrollYTop >= wrapperAbsoluteTop) {
+        window.requestAnimationFrame(() => {
+          TweenLite.set(scene.current, { position: 'fixed' });
+        })
+      } else if (scene.current.style.position === 'fixed') {
+        window.requestAnimationFrame(() => {
+          TweenLite.set(scene.current, { position: 'sticky' });
+        })
+      }
+
+      for (let i = 0; i <= 3; i++) {
+        let minScroll = wrapperAbsoluteTop + chapterHeight * (i - 0.33);
+        let maxScroll = wrapperAbsoluteTop + chapterHeight * (i + 0.67);
+
+        if (scrollYTop > minScroll && scrollYTop < maxScroll) {
+          if (parseFloat(chapters[i].style.opacity) === 0) {
+            TweenLite.to(chapters[i], 0.4, { opacity: 1, y: '0%', ease: Power2.easeInOut });
+            svgAnimation.playSegments(svgAnimationSegments[i], true);
+          }
+        } else {
+          if (parseFloat(chapters[i].style.opacity) > 0) {
+            const tl = new TimelineLite();
+            tl.to(chapters[i], 0.4, { opacity: 0, y: '-25%', ease: Power2.easeInOut })
+              .set(chapters[i], { y: '25%' });
+          }
+        }
+      }
+    }
+  }
+
+  function initComponent() {
+    chapters = Array.from(document.querySelectorAll('.story__chapter'));
+
+    if (window.innerWidth >= breakpoints.medium) {
+      TweenLite.set(wrapper.current, { height: '400vh' });
+      TweenLite.set(scene.current, { position: 'sticky', top: 0, left: 0, height: '100vh', width: '100%' });
+      TweenLite.set(illustration.current, { display: 'block', position: 'absolute', top: 0, left: 0 });
+      TweenLite.set(chapters, { position: 'absolute', top: 0, left: 0, height: '100%', opacity: 0, y: '25%' });
+
+      const wrapperRelativeTop = wrapper.current.getBoundingClientRect().top;
+      const wrapperRelativeBottom = wrapper.current.getBoundingClientRect().bottom;
+      const pageYOffset = window.pageYOffset;
+      wrapperAbsoluteTop = wrapperRelativeTop + pageYOffset;
+      wrapperAbsoluteBottom = wrapperRelativeBottom + pageYOffset;
+
+      chapterHeight = wrapper.current.offsetHeight / 5;
+
+      handleScroll();
+    } else {
+      wrapper.current.removeAttribute('style')
+      scene.current.removeAttribute('style')
+      illustration.current.removeAttribute('style')
+      chapters.forEach(chapter => chapter.removeAttribute('style'))
+
+      svgAnimation.playSegments(
+        [svgAnimationSegments[0][0], svgAnimationSegments[svgAnimationSegments.length - 1][1]]
+        , true)
+    }
+  }
+
+  function markdownToList(markdown) {
+    return (
+      <ReactMarkdown
+        source={markdown}
+        renderers={{
+          listItem: ({ children }) => <li style={{ backgroundImage: `url(${arrows})` }}>{children}</li>,
+        }}
+        allowedTypes={['text', 'listItem']}
+        unwrapDisallowed={true}
+      />
+    );
+  }
 };
 
 export default Story;
-
-function markdownToList(markdown) {
-  return (
-    <ReactMarkdown
-      source={markdown}
-      renderers={{
-        listItem: ({children}) => <li style={{backgroundImage: `url(${arrows})`}}>{children}</li>,
-      }}
-      allowedTypes={['text', 'listItem']}
-      unwrapDisallowed={true}
-    />
-  );
-}
